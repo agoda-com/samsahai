6e692857023c60c9b9ce5d74ae92230565342bcb
Kornchanok Jiravesayakul <Kornchanok.jira@gmail.com>
2020-04-14 15:13:48 +0700

    :bug: [patch] Fix active promotion queue out of reconcile (#21)
    * :bug: Fix active promotion queue out of reconcile
    
    * :white_check_mark: Update test

--- internal/samsahai/activepromotion/queue.go
+++ internal/samsahai/activepromotion/queue.go
@@ -5,38 +5,52 @@ import (
 
 	"github.com/pkg/errors"
 	corev1 "k8s.io/api/core/v1"
-	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	s2hv1beta1 "github.com/agoda-com/samsahai/api/v1beta1"
 )
 
-func (c *controller) manageQueue(ctx context.Context) (bool, error) {
-	atpComps := s2hv1beta1.ActivePromotionList{}
-	if err := c.client.List(ctx, &atpComps, &client.ListOptions{}); err != nil {
-		if k8serrors.IsNotFound(err) {
-			return false, nil
-		}
-
+func (c *controller) manageQueue(ctx context.Context, currentAtpComp *s2hv1beta1.ActivePromotion) (bool, error) {
+	runningAtpComps := s2hv1beta1.ActivePromotionList{}
+	listOpts := client.ListOptions{LabelSelector: labels.SelectorFromSet(c.getStateLabel(stateRunning))}
+	if err := c.client.List(ctx, &runningAtpComps, &listOpts); err != nil {
 		return false, errors.Wrap(err, "cannot list activepromotions")
 	}
 
-	atpComps.SortASC()
-
 	concurrentAtp := c.configs.ActivePromotion.Concurrences
-	for i, pcount := 0, 0; i < len(atpComps.Items) && pcount < concurrentAtp; i, pcount = i+1, pcount+1 {
-		if atpComps.Items[i].Status.State == s2hv1beta1.ActivePromotionWaiting {
-			logger.Info("start active promotion process", "team", atpComps.Items[i].Name)
-			logger.Debug("start creating pre-active environment", "team", atpComps.Items[i].Name)
+	if len(runningAtpComps.Items) >= concurrentAtp {
+		return false, nil
+	}
 
-			c.addFinalizer(&atpComps.Items[i])
-			atpComps.Items[i].SetState(s2hv1beta1.ActivePromotionCreatingPreActive,
-				"Creating pre-active environment")
-			atpComps.Items[i].Status.SetCondition(s2hv1beta1.ActivePromotionCondStarted, corev1.ConditionTrue,
-				"Active promotion has been started")
-			if err := c.updateActivePromotion(ctx, &atpComps.Items[i]); err != nil {
-				return false, err
-			}
+	waitingAtpComps := s2hv1beta1.ActivePromotionList{}
+	listOpts = client.ListOptions{LabelSelector: labels.SelectorFromSet(c.getStateLabel(stateWaiting))}
+	if err := c.client.List(ctx, &waitingAtpComps, &listOpts); err != nil {
+		return false, errors.Wrap(err, "cannot list activepromotions")
+	}
+
+	// there is no new queue
+	if len(waitingAtpComps.Items) == 0 {
+		return false, nil
+	}
+
+	waitingAtpComps.SortASC()
+
+	if concurrentAtp-len(runningAtpComps.Items) > 0 {
+		logger.Info("start active promotion process", "team", waitingAtpComps.Items[0].Name)
+
+		c.addFinalizer(&waitingAtpComps.Items[0])
+		waitingAtpComps.Items[0].SetState(s2hv1beta1.ActivePromotionCreatingPreActive,
+			"Creating pre-active environment")
+		waitingAtpComps.Items[0].Status.SetCondition(s2hv1beta1.ActivePromotionCondStarted, corev1.ConditionTrue,
+			"Active promotion has been started")
+		waitingAtpComps.Items[0].Labels = c.getStateLabel(stateRunning)
+		if err := c.updateActivePromotion(ctx, &waitingAtpComps.Items[0]); err != nil {
+			return false, err
+		}
+
+		// should not continue the process due to current active promotion component has been updated
+		if waitingAtpComps.Items[0].Name == currentAtpComp.Name {
 			return true, nil
 		}
 	}
