c97c3d4329ffd4a9383a32b93dc8b851b35ba21a
sunny299 <sunny_petchapan@hotmail.com>
2020-09-03 15:26:36 +0700

    update teamcity noti message

--- internal/staging/start_testrunner.go
+++ internal/staging/start_testrunner.go
@@ -55,13 +55,14 @@ func (c *controller) checkTestTimeout(queue *s2hv1beta1.Queue, testingTimeout me
 	// check testing timeout
 	if queue.Status.StartTestingTime != nil &&
 		now.Sub(queue.Status.StartTestingTime.Time) > testingTimeout.Duration {
+
+		// set teamcity build number to message
+		queue.Status.TestRunner.Teamcity.BuildNumber = s2herrors.ErrBuildCannotBeTriggeredInTime.Error()
+
 		// testing timeout
 		if err := c.updateTestQueueCondition(queue, v1.ConditionFalse, "queue testing timeout"); err != nil {
 			return err
 		}
-
-		// set teamcity build number to message
-		queue.Status.TestRunner.Teamcity.BuildNumber = s2herrors.ErrBuildCannotBeTriggeredInTime.Error()
 
 		logger.Error(s2herrors.ErrTestTimeout, "test timeout")
 		return s2herrors.ErrTestTimeout
