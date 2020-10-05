02b90b387f74b53033b4d1989127c457c876c354
Kornchanok Jiravesayakul <Kornchanok.jira@gmail.com>
2020-10-05 13:23:55 +0700

    🍱  [patch] Support teamcity test runner with PR branch (#69)
    * :bento: Support test runner with pr branch
    
    * :white_check_mark: Add teamcity test runner test

--- test/e2e/staging/ctrl.go
+++ test/e2e/staging/ctrl.go
@@ -673,7 +673,8 @@ var _ = Describe("[e2e] Staging controller", func() {
 		By("Ensure Pull Request Components")
 		err = wait.PollImmediate(2*time.Second, deployTimeout, func() (ok bool, err error) {
 			retry := 0
-			queue, err := queue.EnsurePullRequestComponents(client, teamName, namespace, redisCompName, prComps, retry)
+			queue, err := queue.EnsurePullRequestComponents(client, teamName, namespace, redisCompName, "123",
+				prComps, retry)
 			if err != nil {
 				logger.Error(err, "cannot ensure pull request components")
 				return false, nil
