f6e9f20d8a43320c829895ac55d4a7a36b666795
Punyawat Rujipiranunt <rew150@outlook.com>
2021-12-16 16:27:38 +0700

    test: add time

--- test/e2e/pullrequest/ctrl.go
+++ test/e2e/pullrequest/ctrl.go
@@ -47,7 +47,7 @@ const (
 	verifyTime30s          = 30 * time.Second
 	verifyTime45s          = 45 * time.Second
 	verifyTime60s          = 60 * time.Second
-	verifyTime75s          = 75 * time.Second
+	verifyTime90s          = 90 * time.Second
 	verifyTime120s         = 120 * time.Second
 	verifyNSCreatedTimeout = verifyTime30s
 )
@@ -322,7 +322,7 @@ var _ = Describe("[e2e] Pull request controller", func() {
 		Expect(err).NotTo(HaveOccurred(), "Verify PullRequestTrigger error")
 
 		By("Verifying PullRequestQueue has been created and PullRequestTrigger has been deleted")
-		err = wait.PollImmediate(verifyTime1s, verifyTime75s, func() (ok bool, err error) {
+		err = wait.PollImmediate(verifyTime1s, verifyTime90s, func() (ok bool, err error) {
 			prQueue := s2hv1.PullRequestQueue{}
 			err = client.Get(ctx, types.NamespacedName{Name: bundledPRTriggerName, Namespace: stgNamespace}, &prQueue)
 			if err != nil {
@@ -450,7 +450,7 @@ var _ = Describe("[e2e] Pull request controller", func() {
 		Expect(strings.Contains(prQueueHistList.Items[0].Name, bundledPRTriggerName)).To(BeTrue())
 		Expect(prQueueHistList.Items[0].Spec.PullRequestQueue).NotTo(BeNil())
 		Expect(prQueueHistList.Items[0].Spec.PullRequestQueue.Status.Result).To(Equal(s2hv1.PullRequestQueueSuccess))
-	}, 150)
+	}, 175)
 
 	It("should successfully deploy pull request queue with 1 component and dependencies", func(done Done) {
 		defer close(done)
